141afe9871126ad6fe3c73d41b15f21ce92b1820
Kavini <89444973+Kavini13@users.noreply.github.com>
2023-05-08 14:49:56 -0700

    Update ass2.shapes.py

--- ass2.shapes.py
+++ ass2.shapes.py
@@ -1,17 +1,39 @@
-
-
 class CircleShape():
-    ''' 
-     A class to represent a circle'''
-    def __init__(self,radius):
+
+    def __init__(self, radius):
         self.my_radius = radius
 
     def area(self):
-        return self.my_radius**2*3.14
-    
+        return self.my_radius ** 2 * 3.14
+
     def perimeter(self):
-        return 2*self.my_radius*3.14
-    
-Newcircle = CircleShape(7)
-print(Newcircle.area())
-print(Newcircle.perimeter())
+        return 2 * self.my_radius * 3.14
+
+
+newCircle = CircleShape(8)
+print(newCircle.area())
+print(newCircle.perimeter())
+
+
+class Rectangle():
+    def __init__(self, length, breadth):
+        self.length = length
+        self.breadth = breadth
+
+    def rectangle_area(self):
+        return self.length * self.breadth
+
+    def rectangle_perimeter(self):
+        return 2*self.length + 2*self.breadth
+
+
+newRectangle = Rectangle(12
+10)
+print(newRectangle.rectangle_
+area())
+print(newRectangle.rectangle_perimeter())
+
+
+class Triangle:
+    def __init__(self,):
+
