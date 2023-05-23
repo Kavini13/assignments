42af29b16e2bfeda2afaf65350266f5d5e66df32
Kavini <89444973+Kavini13@users.noreply.github.com>
2023-05-23 09:49:17 +0530

    Delete Fast api assignment/pythonProject9 directory

--- Fast api assignment/pythonProject9/scripts/core/db/mongodb.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient  # import mongo client to connect
-
-# Creating instance of mongo client
-client = MongoClient("mongodb://intern_23:intern%40123@192.168.0.220:2717/interns_b2_23")
-# Creating database
-database = client.interns_b2_23
-
-# creating document
-lib = database.kavinis
